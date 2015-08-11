8db5f1d9b3017bdeb847ab1b857a4131d6c47f16
marieashley_78 <marieashley_78@hotmail.com>
2015-08-11 02:11:06 -0400

    commit validating form data

--- js/main.js
+++ js/main.js
@@ -6,3 +6,67 @@
 // 3. Connect the validation object to an event handler tied to the submit button.
 
 // Refer to the `index.html` file for the validation rules that must be enforced.
+
+/* $( document ).ready (function() 
+	$(".formvalidation").validate({
+		rules: {
+			name: "required",
+			streetaddress: "required",
+			city: "required",
+			state: "required",
+			zip: "required",
+			},
+			
+			
+
+		messages: {
+			name: "Please enter your name"
+			},
+});			
+					 
+*/
+
+/* Custom JS validation exercise */
+
+$(document).on('ready', function(){
+    // Form validation
+    $('#order-form').validate({
+        submitHandler: function(form) {
+            // If form is valid, submit it!
+            form.submit();
+        },
+        rules: {
+            "your-name": {
+                required: true,
+                maxlength: 128
+            },
+            "your-state": {
+                required: true,
+                maxlength: 2
+            },
+            "your-zip": {
+                required: true,
+                maxlength: 5,
+                digits: true
+            },
+            "card-holder-name": {
+                required: true,
+                maxlength: 128
+            },
+            "card-number": {
+                required: true,
+                creditcard: true
+            },
+            "cvv": {
+                required: true,
+                maxlength: 3,
+                digits: true
+            }
+        }
+    });
+    // Tooltips
+    $('label span.glyphicon').tooltip();
+});
+
+
+				
